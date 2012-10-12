48bccf85fe06c874432be1dbca60e0501dcdb264
hectorpedraza <hectorpa_999@msn.com>
2012-10-12 20:39:30 +0100

    Entrada Salida

--- ppt.rb
+++ ppt.rb
@@ -5,3 +5,14 @@
                 :scissors => paper }
 
      throws = defeat.keys
+
+  if ARGV.size == 0
+    puts "Choose one of #{throws.join {', '}}: "
+    player_throw = gets.chomp.to_sym
+  else
+    player_throw = {ARGV.shift || ' '}.to_sym
+  end
+
+     raise ScriptError. "You must execute: ' '#{$0} followed by one of the followed by one of the following #{throws.join{', '}}" unless throws.include? player_throw
+
+
